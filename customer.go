8346314024bdcbc2d675b67a0d6d1aaf6d812392
James Palawaga <jamesp@polytomic.com>
2022-10-13 17:59:54 -0400

    Create new ID lookup method, rename things for clarity

--- customer.go
+++ customer.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -25,7 +26,7 @@ type Customer struct {
 	ID         string                 `json:"id,omitempty"`
 }
 
-type searchResponse struct {
+type attributesResponse struct {
 	Customer struct {
 		Attributes struct {
 			Attributes string `json:"attributes"`
@@ -37,7 +38,7 @@ type searchResponse struct {
 	} `json:"customer"`
 }
 
-func (c *APIClient) CustomerSearch(ctx context.Context, id string, idType IdentifierType) (Customer, error) {
+func (c *APIClient) GetCustomer(ctx context.Context, id string, idType IdentifierType) (Customer, error) {
 	v := url.Values{}
 	v.Add("id_type", string(idType))
 	qs := v.Encode()
@@ -52,7 +53,7 @@ func (c *APIClient) CustomerSearch(ctx context.Context, id string, idType Identi
 	} else if statusCode != http.StatusOK {
 		return Customer{}, &CustomerIOError{status: statusCode, url: url, body: body}
 	}
-	resp := searchResponse{}
+	resp := attributesResponse{}
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		return Customer{}, err
@@ -83,6 +84,85 @@ func (c *APIClient) CustomerSearch(ctx context.Context, id string, idType Identi
 		Email:      resp.Customer.Attributes.Email,
 		ID:         resp.Customer.Attributes.ID,
 	}, nil
+}
+
+type customerSearchRequest struct {
+	Filter filterCondition `json:"filter"`
+}
+type filterCondition struct {
+	Or  []attributeCondition `json:"or,omitempty"`
+	And []attributeCondition `json:"and,omitempty"`
+}
+type attributeCondition struct {
+	Attribute attribute `json:"attribute"`
+}
+
+type attribute struct {
+	Field    string `json:"field"`
+	Operator string `json:"operator"`
+	Value    string `json:"value"`
+}
+
+type searchResponse struct {
+	Identifiers []map[string]interface{} `json:"identifiers"`
+}
+
+// NewEqAttribute takes a field and string and produces an Equality
+// AttributeCondition
+func NewEqAttribute(field string, value string) attributeCondition {
+	return attributeCondition{
+		Attribute: attribute{
+			Field:    field,
+			Operator: "eq",
+			Value:    value,
+		},
+	}
+}
+
+// LookupCustomerIds takes a list of emails/ids/cio ids and returns a list of
+// the same size with the valid (if any) cio ids.
+func (c *APIClient) LookupCustomerioIds(ctx context.Context, ids []string, idType IdentifierType) ([]string, error) {
+	// A better thing to do would be to split these into batches and then issue
+	// requests, one per 1k results. This is just a nicety at this point, so
+	// I'll leave that for another time.
+	if len(ids) > 1000 {
+		return nil, errors.New("Can only lookup 1k customers at a time")
+	}
+	conditions := make([]attributeCondition, len(ids))
+	for i, id := range ids {
+		conditions[i] = NewEqAttribute(string(idType), id)
+	}
+	payload := customerSearchRequest{
+		Filter: filterCondition{Or: conditions},
+	}
+	url := "/v1/customers?limit=1000"
+	body, statusCode, err := c.doRequest(ctx, "POST", url, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	if statusCode != http.StatusOK {
+		return nil, &CustomerIOError{status: statusCode, url: url, body: body}
+	}
+	resp := searchResponse{}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	lookup := map[string]string{}
+	for _, result := range resp.Identifiers {
+		lookup[fmt.Sprint(result[string(idType)])] = fmt.Sprint(result["cio_id"])
+	}
+
+	result := make([]string, len(ids))
+	for i, id := range ids {
+		if idType == IdentifierTypeEmail {
+			id = strings.ToLower(id)
+		}
+		result[i] = lookup[id]
+	}
+	return result, nil
 }
 
 type emailSearchResponse struct {
